fbdc1e55c8af5cfa87a20456b60dfc0b35725572
rakibtg <rakibtg@gmail.com>
2018-04-13 00:07:59 +0600

    New deign

--- site/pages/homepage.php
+++ site/pages/homepage.php
@@ -1,17 +1,17 @@
 <div class="article-lists">
 
   <div class="article-snippet">
-    <h1><a href="">This is a title</a></h1>
+    <h1><a class='main-title' href="">This is a title</a></h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores minima repellendus, perspiciatis temporibus velit consectetur totam voluptates adipisci non quisquam cupiditate culpa a suscipit. Debitis vero assumenda ab soluta sunt.</p>
   </div>
 
   <div class="article-snippet">
-    <h1><a href="">This is a title</a></h1>
+    <h1><a class='main-title' href="">This is a title</a></h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores minima repellendus, perspiciatis temporibus velit consectetur totam voluptates adipisci non quisquam cupiditate culpa a suscipit. Debitis vero assumenda ab soluta sunt.</p>
   </div>
 
   <div class="article-snippet">
-    <h1><a href="">This is a title</a></h1>
+    <h1><a class='main-title' href="">This is a title</a></h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores minima repellendus, perspiciatis temporibus velit consectetur totam voluptates adipisci non quisquam cupiditate culpa a suscipit. Debitis vero assumenda ab soluta sunt.</p>
   </div>
 
