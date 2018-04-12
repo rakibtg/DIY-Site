e893a9356f4197a7cbfbbba6a84e108b10676a8c
rakibtg <rakibtg@gmail.com>
2018-04-13 00:07:27 +0600

    New login form

---
+++ site/admin/login.php
@@ -0,0 +1,14 @@
+<?php
+  $title = 'Login';
+?>
+
+<div class="login-form-wrapper">
+  <div class="login-form">
+    <h1 class='main-title'>Login</h1>
+    <form action="">
+      <div><input type="text" class="input-control" placeholder="Email address"></div>
+      <div><input type="password" class="input-control" placeholder="Password"></div>
+      <div><button class="button-control">Login</button></div>
+    </form>
+  </div>
+</div>
