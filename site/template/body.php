a01285fcfc29987a731b4aadfcbb8b7bc8f5240c
rakibtg <rakibtg@gmail.com>
2018-04-13 00:08:12 +0600

    Changes for static path

--- site/template/body.php
+++ site/template/body.php
@@ -1,5 +1,7 @@
 <div class="container content-body">
-  <?php
-    require_once './site/pages/'. $page .'.php';
-  ?>
+  <div class="col-sm-2"></div>
+  <div class="col-sm-8">
+    <?php require_once $page; ?>  
+  </div>
+  <div class="col-sm-2"></div>
 </div>
