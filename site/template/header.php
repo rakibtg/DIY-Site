e36769d2b906250edb000e763d060ad124550838
rakibtg <rakibtg@gmail.com>
2018-04-13 00:08:21 +0600

    Added site title

--- site/template/header.php
+++ site/template/header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title><?php echo $title; ?></title>
   <link rel="stylesheet" href="./site/assets/app.css">
   <link rel="stylesheet" href="./site/assets/bootstrap.min.css">
 </head>
@@ -12,7 +12,7 @@
   <div id="header">
     <div class="header-wrapper container">
       <div class="header-content">
-        <h1>DIY Site</h1>
+        <h1><a href='/'>DIY Site</a></h1>
         <p>Welcome to DIY Static Site Generator!</p>
       </div>
     </div>
